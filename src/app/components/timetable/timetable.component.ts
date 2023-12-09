e9d0b55668de01d01c45930c18ae71c88b9f1c22
reda <redamakaoui2001@gmail.com>
2023-12-09 13:36:39 +0100

    last modif gestion

--- src/app/components/timetable/timetable.component.ts
+++ src/app/components/timetable/timetable.component.ts
@@ -12,36 +12,355 @@ import {CookieService} from "ngx-cookie-service";
 export class TimetableComponent implements OnInit {
   events = [
     {
-      title: 'Developpement JEE',
-      start: '2023-12-05T14:30:00',
-      backgroundColor: 'blue',
+      title: 'DevOps',
+      start: '2023-11-27T14:30:00',
+      backgroundColor: '#8EACCD',
       textColor: 'white',
       borderColor: 'black',
       extendedProps: {
-        instructor: 'm.lachgar',
+        instructor: 'N.Assad',
+        location: 'B11',
+      },
+    },
+    {
+      title: 'Encadrement',
+      start: '2023-11-27T10:30:00',
+      backgroundColor: '#D7E5CA',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'm.Lachgar',
+        location: 'home',
+      },
+    },
+    {
+      title: 'microservices',
+      start: '2023-11-28T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'c.baidada',
         location: 'A1',
       },
     },
     {
-      title: 'cloud',
-      start: '2023-12-05T10:30:00',
-      backgroundColor: 'green',
-      textColor: 'white',
-      borderColor: 'black',
-      extendedProps: {
-        instructor: 'A.aqqal',
-        location: 'A2',
-      },
-    },
-    {
-      title: 'machine learning',
-      start: '2023-12-06T14:30:00',
-      backgroundColor: 'blue',
+      title: 'fouille et visualisation big data',
+      start: '2023-11-28T14:30:00',
+      backgroundColor: '#8EACCD',
       textColor: 'white',
       borderColor: 'black',
       extendedProps: {
         instructor: 'f.kalloubi',
         location: 'A1',
+      },
+    },
+    {
+      title: 'architecture BigData',
+      start: '2023-11-29T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'f.kalloubi',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'securité cloud',
+      start: '2023-11-29T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'm.Boujnouni',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'Aassurance qualité logiciel',
+      start: '2023-11-30T10:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'A.hannani',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'recherche scientifique',
+      start: '2023-12-01T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'c.Razouk',
+        location: 'A2',
+      },
+    },
+    {
+      title: "recherche d'emploie",
+      start: '2023-12-01T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'JADA',
+        location: 'Amphi 1',
+      },
+    },
+    {
+      title: 'DevOps',
+      start: '2023-12-04T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'N.Assad',
+        location: 'B11',
+      },
+    },
+    {
+      title: 'Encadrement',
+      start: '2023-12-04T10:30:00',
+      backgroundColor: '#D7E5CA',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'm.Lachgar',
+        location: 'home',
+      },
+    },
+    {
+      title: 'microservices',
+      start: '2023-12-05T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'c.baidada',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'fouille et visualisation big data',
+      start: '2023-12-05T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'f.kalloubi',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'architecture BigData',
+      start: '2023-12-06T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'f.kalloubi',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'securité cloud',
+      start: '2023-12-06T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'm.Boujnouni',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'Aassurance qualité logiciel',
+      start: '2023-12-07T10:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'A.hannani',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'recherche scientifique',
+      start: '2023-12-08T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'c.Razouk',
+        location: 'A2',
+      },
+    },
+    {
+      title: "recherche d'emploie",
+      start: '2023-12-08T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'JADA',
+        location: 'Amphi 1',
+      },
+    },
+    {
+      title: 'DevOps',
+      start: '2023-12-11T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'N.Assad',
+        location: 'B11',
+      },
+    },
+    {
+      title: 'Encadrement',
+      start: '2023-12-11T10:30:00',
+      backgroundColor: '#D7E5CA',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'm.Lachgar',
+        location: 'home',
+      },
+    },
+    {
+      title: 'microservices',
+      start: '2023-12-12T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'c.baidada',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'fouille et visualisation big data',
+      start: '2023-12-12T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'f.kalloubi',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'architecture BigData',
+      start: '2023-12-13T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'f.kalloubi',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'securité cloud',
+      start: '2023-12-13T14:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'm.Boujnouni',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'Aassurance qualité logiciel',
+      start: '2023-12-14T10:30:00',
+      backgroundColor: '#D7E5CA',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'A.hannani',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'recherche scientifique',
+      start: '2023-12-15T08:30:00',
+      backgroundColor: '#8EACCD',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'c.Razouk',
+        location: 'A2',
+      },
+    },
+    {
+      title: "recherche d'emploie",
+      start: '2023-12-15T14:30:00',
+      backgroundColor: '#D7E5CA',
+      textColor: 'white',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'JADA',
+        location: 'Amphi 1',
+      },
+    },
+    {
+      title: 'architecture BigData',
+      start: '2023-12-26T08:30:00',
+      backgroundColor: '#F9F3CC',
+      textColor: 'black',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'f.kalloubi',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'securité cloud',
+      start: '2023-12-27T14:30:00',
+      backgroundColor: '#F9F3CC',
+      textColor: 'black',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'm.Boujnouni',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'Aassurance qualité logiciel',
+      start: '2023-12-28T10:30:00',
+      backgroundColor: '#F9F3CC',
+      textColor: 'black',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'A.hannani',
+        location: 'A1',
+      },
+    },
+    {
+      title: 'recherche scientifique',
+      start: '2023-12-28T08:30:00',
+      backgroundColor: '#F9F3CC',
+      textColor: 'black',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'c.Razouk',
+        location: 'A2',
+      },
+    },
+    {
+      title: "recherche d'emploie",
+      start: '2023-12-27T14:30:00',
+      backgroundColor: '#F9F3CC',
+      textColor: 'black',
+      borderColor: 'black',
+      extendedProps: {
+        instructor: 'JADA',
+        location: 'Amphi 1',
       },
     },
     { }
@@ -95,7 +414,7 @@ export class TimetableComponent implements OnInit {
 
       // Apply styling based on the extendedProps
       content.style.backgroundColor = arg.event['backgroundColor'] || 'blue';
-      content.style.color = additionalProps['textColor'] || 'white';  // Use square brackets for textColor
+      content.style.color = additionalProps['textColor'] || 'black';  // Use square brackets for textColor
       content.style.borderColor = additionalProps['borderColor'] || 'black';  // Use square brackets for borderColor
     }
 
